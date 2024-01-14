1223ba17ba409e4ecded29b3e3fe014874b1455c
Kamil <Daba9889@gmail.com>
2024-01-14 17:16:37 +0300

    Off header blur, fix header open and close menu, off blur of main image, style scrollbar

--- frontend/src/components/navMenu/navMenu.tsx
+++ frontend/src/components/navMenu/navMenu.tsx
@@ -9,25 +9,29 @@ import SettingYellow from '$/public/svg/settingYellow.svg'
 import './navMenu.css'
 const NavMenu = () => {
     const [menuSate, setMenuState] = useState(false);
-  return (
-    <div onClick={() => { setMenuState(!menuSate) }} className='header__nav-menu'>
-                    <Image alt='user icon' src={User} className="header__user" />
-                    <div className={menuSate ? "header__nav-card header__nav-card-active" : "header__nav-card"}>
-                        <ul>
-                            <li>
-                                
-                                <Link href={'/account'}><Image height={27} width={27} src={LoginUser} alt={'login user'}/>Личный кабинет</Link>
-                            </li>
-                            <li>
-                                <Link href={'/account/settings'}><Image height={27} width={27} src={SettingYellow} alt={'settings yellow'}/>Настройки</Link>
-                            </li>
-                            <li>
-                                <Link href={'/login'}><Image height={27} width={27} src={Exit} alt={'exit'}/>Выйти с аккаунта</Link>
-                            </li>
-                        </ul>
-                    </div>
+    return (
+        <>
+            <div onClick={() => { setMenuState(!menuSate) }} className={menuSate ? "nav-menu__Wrapper nav-menu__Wrapper-active" : "nav-menu__Wrapper"}></div>
+            <div className='header__nav-menu'>
+                <Image onClick={() => { setMenuState(!menuSate) }} alt='user icon' src={User} className="header__user" />
+                <div className={menuSate ? "header__nav-card header__nav-card-active" : "header__nav-card"}>
+                    <ul>
+                        <li>
+
+                            <Link href={'/account'}><Image height={27} width={27} src={LoginUser} alt={'login user'} />Личный кабинет</Link>
+                        </li>
+                        <li>
+                            <Link href={'/account/settings'}><Image height={27} width={27} src={SettingYellow} alt={'settings yellow'} />Настройки</Link>
+                        </li>
+                        <li>
+                            <Link href={'/login'}><Image height={27} width={27} src={Exit} alt={'exit'} />Выйти с аккаунта</Link>
+                        </li>
+                    </ul>
                 </div>
-  )
+            </div>
+        </>
+
+    )
 }
 
 export default NavMenu
